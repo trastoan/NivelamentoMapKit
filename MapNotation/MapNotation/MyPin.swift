9a8faae1d573f6a3d7453dfcb467c7d0b8b16d9f
trastoan <yurisaboiaf@gmail.com>
2016-09-17 12:30:03 -0300

    First Commit to project

---
+++ MapNotation/MapNotation/MyPin.swift
@@ -0,0 +1,75 @@
+//
+//  MyPin.swift
+//  MapNotation
+//
+//  Created by Yuri Saboia Felix Frota on 9/13/16.
+//  Copyright © 2016 Yuri Saboia Felix Frota. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MyPin: NSObject, MKAnnotation {
+    let title : String?
+    let locationName : String
+    let coordinate: CLLocationCoordinate2D
+    let annotationImage : UIImage
+    let pokemonImage : UIImage
+    
+    init(withTitle title:String, andLocationName locationName:String, andCoordinate coordinate: CLLocationCoordinate2D, andAnnotationImage image:UIImage) {
+        self.title = title
+        self.locationName = locationName
+        self.coordinate = coordinate
+        self.annotationImage = UIImage(named: "poke")!
+        self.pokemonImage = image
+    }
+    
+    
+    var annotationView : MKAnnotationView? {
+        let view = MKAnnotationView(annotation: self, reuseIdentifier: "myCustomAnnotation")
+        view.image = self.annotationImage
+        view.isEnabled = true
+        view.canShowCallout = true
+        view.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
+        imageView.image = self.pokemonImage
+        
+        view.leftCalloutAccessoryView = imageView
+        
+        return view
+    }
+    var subtitle: String? {
+        return locationName
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ MapNotation/MapNotation/ViewController.swift
@@ -0,0 +1,165 @@
+//
+//  ViewController.swift
+//  MapNotation
+//
+//  Created by Yuri Saboia Felix Frota on 9/13/16.
+//  Copyright © 2016 Yuri Saboia Felix Frota. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+
+    @IBOutlet weak var mapView: MKMapView!
+    var myLocation : CLLocationCoordinate2D?
+    let locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        let longGesture = UILongPressGestureRecognizer(target: self, action: #selector(ViewController.addMyPoint))
+        longGesture.minimumPressDuration = 1.5
+        
+        self.mapView.addGestureRecognizer(longGesture)
+        
+        self.locationManager.delegate = self
+        self.mapView.delegate = self
+        
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+            locationManager.requestLocation()
+            self.mapView.showsUserLocation = true
+        }
+    }
+    
+    func checkLocalizationPermission() {
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedWhenInUse:
+            print("Autorizado em uso")
+        case .authorizedAlways:
+            print("Autorizado Sempre")
+        case .denied:
+            print("Negado")
+        case .notDetermined:
+            self.locationManager.requestWhenInUseAuthorization()
+            print("Não determinado")
+        case .restricted:
+            print("Restrito")
+        }
+    }
+    
+    func createAlert(_ text : String,andTitle title: String) {
+        let alert = UIAlertController(title: title, message: text, preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: { action in
+            alert.dismiss(animated: true, completion: nil)
+        }))
+        self.present(alert, animated: true, completion: nil)
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func addMyPoint(press : UIGestureRecognizer) {
+        if press.state == .began{
+            let locationOnView = press.location(in: self.mapView)
+            let newCoordinates = self.mapView.convert(locationOnView, toCoordinateFrom: self.mapView)
+            
+            CLGeocoder().reverseGeocodeLocation(CLLocation(latitude: newCoordinates.latitude, longitude: newCoordinates.longitude), completionHandler: { (placemarks, error) in
+                if error == nil{
+                    if let placemark = placemarks?.first{
+                        if let title = placemark.areasOfInterest?.first{
+                            if placemark.thoroughfare != nil{
+                            let myPin = MyPin(withTitle: title, andLocationName: placemark.thoroughfare!, andCoordinate: newCoordinates, andAnnotationImage: UIImage(named: "poke")!)
+                                self.mapView.addAnnotation(myPin)
+                            }else{
+                                let myPin = MyPin(withTitle: title, andLocationName: "unknow address", andCoordinate: newCoordinates, andAnnotationImage: UIImage(named: "poke")!)
+                                self.mapView.addAnnotation(myPin)
+                            }
+                        }else if let title = placemark.thoroughfare{
+                            let myPin = MyPin(withTitle: "myLocation", andLocationName: title, andCoordinate: newCoordinates, andAnnotationImage: UIImage(named: "poke")!)
+                            self.mapView.addAnnotation(myPin)
+                        }else{
+                            let myPin = MyPin(withTitle: "myLocation", andLocationName: "unknowLocation", andCoordinate: newCoordinates, andAnnotationImage: UIImage(named: "poke")!)
+                            self.mapView.addAnnotation(myPin)
+                        }
+                    }
+                }
+            })
+            
+        }
+    }
+    
+    @IBAction func pinMyLocation(_ sender: AnyObject) {
+        //Adds a pin to the center of the map
+//        let pin = MyPin(withTitle: "New pin", andLocationName: "Something Happening Here", andCoordinate: self.mapView.centerCoordinate)
+//        self.mapView.addAnnotation(pin)
+    }
+
+    
+    //Changes map style
+    @IBAction func selectMap(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            self.mapView.mapType = .standard
+        case 1:
+            self.mapView.mapType = .satellite
+        case 2:
+            self.mapView.mapType = .hybrid
+        default:
+            return
+        }
+    }
+    
+    //Zoom to user location
+    @IBAction func zoomMyLocation(_ sender: AnyObject) {
+        if let myLocation = self.myLocation{
+            let coordinateRegion = MKCoordinateRegionMakeWithDistance(myLocation, 300, 300)
+            mapView.setRegion(coordinateRegion, animated: true)
+        }
+    }
+    
+    //Updating user location on the mapView
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        self.myLocation = locations.first?.coordinate
+    }
+    
+    //Needs to be here
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("ERRO: --- \(error)")
+    }
+    
+    //Center user on screen
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(userLocation.coordinate, 300, 300)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+    //TODO
+    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+//        print(self.mapView.centerCoordinate)
+    }
+    
+    //adding custom Annotation
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if let customAnnotation = annotation as? MyPin{
+            return customAnnotation.annotationView!
+        }else{
+            return nil
+        }
+    }
+//
+//    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+//        <#code#>
+//    }
+//    
+//    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+//        <#code#>
+//    }
+    
+}
+
